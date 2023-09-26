1abb379782f70bac1df3bb4f44d83d37c4b2c910
Johannes Coetsee <johannes.coetsee@ktu.edu>
2023-09-26 23:56:39 +0300

    Initial commit

---
+++ src/main/java/com/example/capstone_petclinic/CapstonePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstonePetClinicApplication.class, args);
+    }
+
+}
